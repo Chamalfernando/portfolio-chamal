f1498a453c9c9ae28f7e3fcebaaef87beef3dffd
Chamalfernando <chamaldeshitha2001@gmail.com>
2025-11-10 08:59:13 +0530

    [FIXES] scroll indicator overlap issue.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -101,7 +101,7 @@ const Hero = () => {
           </motion.div>
 
           <motion.div 
-            className="flex justify-center space-x-6"
+            className="flex justify-center space-x-6 pb-20 sm:pb-0"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.8 }}
